cd0aedb06c665c9f98be0f5561e5bd9d5eb2d013
Pavel Abdi <abdi@awem.com>
2020-04-16 12:59:11 +0300

    Initial commit

---
+++ appmongo.py
@@ -0,0 +1,16 @@
+import time
+from uuid import uuid4
+from storage import MongoService
+
+storage = MongoService.get_instance()
+
+for _ in range(5):
+    dto = {
+        "id": str(uuid4()),
+        "payload": str(uuid4()),
+        "field": int(time.time())
+    }
+    storage.save_data(dto)
+
+for data in storage.get_data():
+    print(data)
